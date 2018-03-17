541cffe8a1c47d383c6e8757ca8e6a857ca1747a
Luciano Gordon <lucianogordon@hotmail.com>
2018-03-17 04:09:02 +0000

    Heroku Updates

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "+h1$1$@r@nd0ms3cr3tk3y"
 #postgresql://luciano:password@localhost/thedatabase
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://qderxnpbebfoos:76999dda31282cbd6572f2e23430fd52e446681a13e11500d34f6d0c0929e530@ec2-54-83-23-91.compute-1.amazonaws.com:5432/db4ocsifq4t575"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://qderxnpbebfoos:76999dda31282cbd6572f2e23430fd52e446681a13e11500d34f6d0c0929e530@ec2-54-83-23-91.compute-1.amazonaws.com:5432/db4ocsifq4t575"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 
