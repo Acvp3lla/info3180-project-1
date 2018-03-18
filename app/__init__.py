2d3d6c638e85d415e0183daef3c9c8f4a9eb2d12
Luciano Gordon <lucianogordon@hotmail.com>
2018-03-18 08:07:59 +0000

    Tethered

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,13 @@ app.config['SECRET_KEY'] = "+h1$1$@r@nd0ms3cr3tk3y"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://luciano:password@localhost/thedatabase"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
-DATABASE_URL='postgresql://mbmqpbvauuqttt:4fcab97e61da14b4b2d52a64346bb3dfaaef1e063bf26fd5b9cfaeff81c4ce89@ec2-50-17-206-214.compute-1.amazonaws.com:5432/dfruam3ori6b22'
+HEROKU_POSTGRESQL_RED_URL='postgresql://ikvjrhcxwgavte:81034d0ca3b4fdfa4ca15262835bfc4b7a1842ff4f0eaa00921319182255f758@ec2-54-163-246-193.compute-1.amazonaws.com:5432/daab9q8cgh9h2a'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_RED_URL']
 
 db = SQLAlchemy(app)
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(HEROKU_POSTGRESQL_RED_URL, sslmode='require')
 
 # Flask-Login login manager
 login_manager = LoginManager()
