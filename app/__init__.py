177f13e434aebb9acc6a20c85a702b3c3a743b09
Luciano Gordon <lucianogordon@hotmail.com>
2018-03-18 07:17:02 +0000

    Tethered

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import os
-import psycopg2
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,10 +7,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "+h1$1$@r@nd0ms3cr3tk3y"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://luciano:password@localhost/thedatabase"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
-
-
-DATABASE_URL='postgres://ikvjrhcxwgavte:81034d0ca3b4fdfa4ca15262835bfc4b7a1842ff4f0eaa00921319182255f758@ec2-54-163-246-193.compute-1.amazonaws.com:5432/daab9q8cgh9h2a'
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 db = SQLAlchemy(app)
 
