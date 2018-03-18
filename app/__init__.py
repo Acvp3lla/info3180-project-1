07fd6564b552718f747af9495844d0f3f775b6dd
Luciano Gordon <lucianogordon@hotmail.com>
2018-03-18 16:44:20 +0000

    heroku db

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a w
 DATABASE_URL='postgresql://gwswplkarpwspj:360fa7639ce174b794a3648c51d99d31045366b0b9899ea04f4906f6872cbd5e@ec2-54-243-185-195.compute-1.amazonaws.com:5432/d59607sinom6t5'
 
 #os.environ[DATABASE_URL]
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ[DATABASE_URL]
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 db = SQLAlchemy(app)
